8f0ebd8d72354a9ba64d5adac115d046daf4c3b4
GrJo <gregor.jochmann@uni-dortmund.de>
2013-12-03 19:05:04 +0100

    first experiments with dynamic meshes and textures

--- TireTracker.cs
+++ TireTracker.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 
 namespace PersistentTrails
 {
-    //TODO make this a partModule
     class ModuleTireTracker : PartModule
     {
         [KSPField(isPersistant = false, guiActive = true, guiName = "Tire Width")]
         public float tireWidth;
 
-        [KSPField(isPersistant = false)]
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Tire Radius")]
         public float tireRadius;
 
         //[KSPField(isPersistant = true)]
@@ -23,14 +22,15 @@ namespace PersistentTrails
     {
         private TireRecorder()
         {
-            tires = new Dictionary<ModuleTireTracker, GameObject>();
+            Debug.Log("TireRecorder constructor");
+            //tires = new Dictionary<ModuleTireTracker, GameObject>();
         }
         public static readonly TireRecorder Instance = new TireRecorder();
 
-        //private GameObject trackContainer;
+        private TireTrail currentTrack;
 
 
-        private Dictionary<ModuleTireTracker, GameObject> tires;
+        //private Dictionary<ModuleTireTracker, GameObject> tires;
         //PartModule trackingModule;
 
         public void update()
@@ -40,136 +40,27 @@ namespace PersistentTrails
             if (FlightGlobals.ActiveVessel == null)
                 return;
 
-            //find all TireTracker modules on current vessel
-            foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+            if (currentTrack == null)
             {
-                //Debug.Log("part in activeVessel: " + p.name);
-
-                foreach (PartModule module in p.Modules.OfType<ModuleTireTracker>())
-                {
-                    ModuleTireTracker tireModule = module as ModuleTireTracker;
-
-                    updateForTire(tireModule);
-                }
-
+                currentTrack = new TireTrail(FlightGlobals.ActiveVessel.transform);
+                Debug.Log("created new track");
             }
+
+            currentTrack.updateMesh(FlightGlobals.ActiveVessel.transform.position);
+            ////find all TireTracker modules on current vessel
+            //foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+            //{
+            //    //Debug.Log("part in activeVessel: " + p.name);
+
+            //    foreach (PartModule module in p.Modules.OfType<ModuleTireTracker>())
+            //    {
+            //        ModuleTireTracker tireModule = module as ModuleTireTracker;
+
+            //        updateForTire(tireModule);
+            //    }
+
+            //}
         }
 
-        //assumes checks for actual movement have already been done
-        private void updateForTire(ModuleTireTracker tire)
-        {
-            //Debug.Log("TireRecorder.updateForTire() " + tire.part.name);
-            //if (!tire.part.GroundContact) //doesnt work
-            //    return;
-
-            //Debug.Log("Part has ground contact!");
-
-            GameObject tireObject;
-            if (!tires.TryGetValue(tire, out tireObject))
-            {
-                //create mesh
-                Debug.Log("Creating new Mesh for tire");
-                Mesh m = new Mesh();
-                m.triangles = new int[0];
-                m.vertices = new Vector3[0];
-
-                tireObject = MeshFactory.makeMeshGameObject(ref m, "TireObject");
-
-                //TODO texture
-                tireObject.renderer.material = new Material(Shader.Find("KSP/Emissive/Diffuse"));
-                tireObject.renderer.renderer.castShadows = false;
-                tireObject.renderer.renderer.receiveShadows = false;
-
-
-                tireObject.renderer.material.SetColor("_EmissiveColor", Color.red);
-
-                tires.Add(tire, tireObject);
-                //.renderer.material.te
-            }
-
-            Mesh mesh = tireObject.GetComponent<MeshFilter>().mesh;
-
-            float width = 2;
-            Vector3 groundContactCenter = tire.part.transform.position;
-            //apply offset
-            groundContactCenter.z -= tire.tireWidth;
-
-            Vector3 tireAxis = tire.part.transform.right;
-
-            int oldVertexCount = mesh.vertexCount;
-
-
-
-
-            Vector3 newLeft = groundContactCenter - width / 2 * tireAxis;
-            Vector3 newRight = groundContactCenter + width / 2 * tireAxis;
-            //have we moved at all?
-            if (oldVertexCount > 0
-                && (newLeft - mesh.vertices[mesh.vertexCount - 2]).sqrMagnitude < 0.1f
-                && (newRight - mesh.vertices[mesh.vertexCount - 1]).sqrMagnitude < 0.1f
-            )
-            {
-                return;
-            }
-
-            //Debug.Log("updating tiretrack-mesh");
-            //add new mesh-vertices 
-
-
-
-
-            Vector3[] newVertices = new Vector3[oldVertexCount + 2];
-            Vector3[] newNormals = new Vector3[oldVertexCount + 2];
-            
-            //copy old vertices
-            for (int i = 0; i < mesh.vertexCount; ++i)
-            {
-                newVertices[i] = mesh.vertices[i];
-                newNormals[i] = mesh.normals[i];
-            }
-
-            //Debug.Log("copied vertices and normals");
-
-            //put 2 new vertices and triangles
-            newVertices[oldVertexCount] = newLeft;
-            newVertices[oldVertexCount + 1] = newRight;
-
-            //Debug.Log("created vertices");
-            int[] newTriangles = {};
-
-            if (oldVertexCount > 2) //first update creates vertices only
-            {
-                Debug.Log("oldVertexCount = " + oldVertexCount + ", newTriangleIndex-ArraySize = " + 3 * (oldVertexCount + 2));
-
-                newTriangles = new int[3 * (oldVertexCount + 2)];
-
-                for (int i = 0; i < mesh.triangles.Length; ++i )
-                    newTriangles[i] = mesh.triangles[i];
-
-                newTriangles[3 * oldVertexCount] = oldVertexCount; //new left upper
-                newTriangles[3 * oldVertexCount + 1] = oldVertexCount - 2; //old last left;
-                newTriangles[3 * oldVertexCount + 2] = oldVertexCount - 1; //old last right
-
-                newTriangles[3 * oldVertexCount + 3] = oldVertexCount + 1; //new right
-                newTriangles[3 * oldVertexCount + 4] = oldVertexCount; //new left
-                newTriangles[3 * oldVertexCount + 5] = oldVertexCount - 1; //old last right
-
-                string indexString = "Triangle indices:";
-                for (int i = 0; i < newTriangles.Length; ++i)
-                    indexString += "[" +i+"] " + newTriangles[i] + ";";
-                Debug.Log(indexString);
-            }
-
-            mesh.Clear();
-            mesh.vertices = newVertices;
-            mesh.normals = newNormals;
-
-            if (oldVertexCount > 2)
-            {
-                mesh.triangles = newTriangles;
-            }
-
-
-        }
     }
 }
---
+++ TireTrail.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PersistentTrails
+{
+    class TireTrail
+    {
+        private string textureName;
+        private GameObject tireObject;
+        private float tireWidth;
+        private float tireCircumfence;
+        private Transform parentOffset;
+
+        private Mesh tireMesh;
+        private List<Vector3> vertices;
+        private List<Vector2> uvCoords;
+        private List<int> triangleVertexIndices;
+
+        private float lastTextureU; // "1" for each full revolution of the wheel
+        private Vector3 lastTirePosition;
+
+        public TireTrail(Transform tireWorld)
+        {
+            Debug.Log("TireTrail constructor");
+            //tirePart.GetReferenceTransform
+            
+            Texture2D texture = Utilities.LoadImage("Tires/default.png", 64, 128);
+            Debug.Log("creating gameobject");
+            tireObject = new GameObject("Dynamic Tire Mesh");
+            MeshFilter filter = tireObject.AddComponent<MeshFilter>();
+            MeshRenderer renderer = tireObject.AddComponent<MeshRenderer>();
+            Debug.Log("setting shadow");
+            renderer.castShadows = false;
+            //tireObject.collider.enabled = false;
+            Debug.Log("setting material tex");
+            renderer.material.mainTexture = texture;
+
+            Debug.Log("setting transform");
+            tireObject.transform.position = tireWorld.position;
+            tireObject.transform.rotation = tireWorld.rotation;
+
+            tireWidth = 2;
+            tireCircumfence = 5;
+            lastTextureU = 0;
+            lastTirePosition = new Vector3(0, 0, 0);
+
+            initializeMesh();
+            filter.mesh = tireMesh;
+        }
+
+        private void initializeMesh()
+        {
+            Debug.Log("creating tiretrail mesh");
+            tireMesh = new Mesh();
+
+            //create 4 vertices for first quad
+            //the mesh will grow into positive x direction, spaced evenly around y
+            //Mesh goes like this
+            // row0 ... row1 ...rowN
+            //  v0      v2   ... v2n
+            //  v1      v3   ... v2n+1
+            
+            vertices = new List<Vector3>{   new Vector3(0, -tireWidth/2),
+                                            new Vector3(0, +tireWidth/2)};
+
+            //create triangles (three indices per face)
+            triangleVertexIndices = new List<int>{};
+
+
+            //texture coordinates - 0,0 is bottom left
+            uvCoords = new List<Vector2>{ new Vector2(0, 1),
+                                          new Vector2(0, 0)};
+
+        }
+
+        public void updateMesh(Vector3 newTirePosition) 
+        {
+            Vector3 newTireRelative = newTirePosition - tireObject.transform.position;
+            //http://docs.unity3d.com/Documentation/ScriptReference/Mesh.html 
+
+            //Also note:Unity supports only 65k triangle arrays
+            float deltaDistance = (lastTirePosition - newTireRelative).magnitude;
+            Debug.Log("Updating tiretrail mesh, new tire pos = " + newTireRelative.ToString() + ", delta to previous is " + deltaDistance + "m");
+
+            lastTirePosition = newTirePosition;
+
+            int oldVertexCount = vertices.Count;
+
+            //two new vertices at new deltaDistance TODO fix rotation etc
+            vertices.Add(new Vector3(newTireRelative.x, newTireRelative.y - tireWidth / 2));
+            vertices.Add(new Vector3(newTireRelative.x, newTireRelative.y + tireWidth / 2));
+
+            //texture coords
+            lastTextureU += deltaDistance / tireCircumfence;
+            Debug.Log("texcoords u=" + lastTextureU);
+            uvCoords.Add(new Vector2(lastTextureU, 1));
+            uvCoords.Add(new Vector2(lastTextureU, 0));
+
+            //triangles
+            //Mesh goes like this
+            // row0 ... row1 ... last old row
+            //  v0      v2   ... v2n
+            //  v1      v3   ... v2n+1
+            triangleVertexIndices.Add(oldVertexCount - 2);
+            triangleVertexIndices.Add(oldVertexCount - 1);
+            triangleVertexIndices.Add(oldVertexCount);
+
+            triangleVertexIndices.Add(oldVertexCount - 1);
+            triangleVertexIndices.Add(oldVertexCount + 1);
+            triangleVertexIndices.Add(oldVertexCount);
+
+            //put it all together
+            tireMesh.vertices = vertices.ToArray();
+            tireMesh.uv = uvCoords.ToArray();
+            tireMesh.triangles = triangleVertexIndices.ToArray();
+
+            //Auto-Normals
+            tireMesh.RecalculateNormals();
+        }
+    }
+}
